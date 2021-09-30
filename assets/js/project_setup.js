d11076279b10507285fcae93185ba7e2f352889d
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-09-30 12:43:36 -0700

    add loader.

--- assets/js/project_setup.js
+++ assets/js/project_setup.js
@@ -60,6 +60,18 @@ ProjectSetup = {
         jQuery.ajax({
             url: ProjectSetup.projectPortalSectionURL,
             type: 'POST',
+            beforeSend: function () {
+                // setting a timeout
+                var data = '<div id="portal-linkage-container">        <div class="alert alert-secondary d-flex justify-content-center">\n' +
+                    '  <div class="spinner-border" role="status">\n' +
+                    '    <span class="sr-only">Loading...</span>\n' +
+                    '  </div>\n' +
+                    '</div></div>'
+
+                setTimeout(function () {
+                    jQuery('#setupChklist-modify_project').before(data);
+                }, 10)
+            },
             success: function (data) {
                 if ($("#portal-linkage-container").length != 0) {
                     $("#portal-linkage-container").replaceWith(data)
