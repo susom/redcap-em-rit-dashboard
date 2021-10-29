08c0e0f0cf7e0062492361cf88dff2c4b683260a
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-10-29 16:51:36 -0700

    replace Contact admin button

--- views/contact_admin_button.php
+++ views/contact_admin_button.php
@@ -5,18 +5,10 @@ namespace Stanford\ProjectPortal;
 
 /** @var \Stanford\ProjectPortal\ProjectPortal $this */
 ?>
-<script src="<?php echo $this->getUrl('assets/js/contact_admin_button.js') ?>"></script>
 <script>
-    ContactButton.linkedProjectURL = "<?php echo $this->getURL('ajax/linked_project.php', false, true) . '&pid=' . $this->getProjectId() ?>"
+    var dashboardUrl = "<?php echo $this->getURL('views/index.php', false, false) ?>"
 
     window.onload = function () {
-        ContactButton.init();
+        $(".btn-contact-admin").attr('href', dashboardUrl).html('<i style="color: white" class="fas fa-desktop"></i> RIT Dashboard')
     };
 </script>
-<div id="warning-dialog" style="top: 10% !important; display: none"
-     title="Link Your project to the new Research Portal">To contact REDCap support team you need to link your REDCap
-    project with Research IT portal project. If you already created Portal Project please click Link below and select
-    your Portal project then click 'Attach Selected Project'. Otherwise please go to <a
-            href="https://rit-portal.med.stanford.edu" target="_blank">https://rit-portal.med.stanford.edu</a> and
-    create your research project.</d></div>
-<div class="loader"><!-- Place at bottom of page --></div>
