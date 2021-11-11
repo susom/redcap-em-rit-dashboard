9ebced0f09297840d480a2123253744fc7479899
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-11-11 14:08:38 -0800

    attempt to build via CLI

---
+++ dashboard/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Project id is: {{this.pid}}</h1>
+    <Test :redcap="redcapPid"></Test>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test'
+export default {
+  name: 'App',
+  components:{
+    'Test' : Test
+  },
+  props: ['pid'],
+  data(){
+    return {
+      'redcapPid': '123'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ dashboard/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  Hello From Test {{this.redcap}}
+</div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: ['redcap']
+}
+</script>
+
+<style scoped>
+
+</style>
