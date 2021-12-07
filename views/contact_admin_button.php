41ce9b373913fd3f12066aefb54072b542c6017a
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-12-07 10:57:19 -0800

    change to api link.

--- views/contact_admin_button.php
+++ views/contact_admin_button.php
@@ -6,7 +6,7 @@ namespace Stanford\ProjectPortal;
 /** @var \Stanford\ProjectPortal\ProjectPortal $this */
 ?>
 <script>
-    var dashboardUrl = "<?php echo $this->getURL('views/index.php', false, false) ?>"
+    var dashboardUrl = "<?php echo $this->getURL('views/index.php', false, true) ?>"
 
     window.onload = function () {
         $(".btn-contact-admin").attr('href', dashboardUrl).html('<i style="color: white" class="fas fa-desktop"></i> REDCap R2P2 Dashboard')
