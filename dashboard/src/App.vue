d782909804fb347e14d359948fe5d59a544c4314
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-12-23 10:19:37 -0800

    add r2p2 tab

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -77,6 +77,12 @@
         </b-tabs>
       </div>
     </b-overlay>
+    <p>
+      <i>We appreciate your patience -- R2P2 is new and we look forward to
+        <a href="https://redcap.stanford.edu/surveys/?s=DNFNAKARFTELJ4WA" target="_blank">
+          incorporating your feedback
+        </a> to make it better! <a href=""></a></i>
+    </p>
   </div>
 </template>
 
