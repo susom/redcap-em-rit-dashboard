da478a0c50762656652d72efe7aa396baaaa6b99
Ihab Zeedia <ihab.zeedia@gmail.cm>
2022-01-06 13:22:51 -0800

    revert debug.

--- ajax/support/create_jira_ticket.php
+++ ajax/support/create_jira_ticket.php
@@ -9,29 +9,29 @@ use GuzzleHttp\Exception\GuzzleException;
 
 try {
     $body = json_decode(file_get_contents('php://input'), true);
-//    $portalProjectId = $body['project_portal_id'];
-//    $redcapProjectId = $body['redcap_project_id'];;
-//    if (!isset($body['redcap-project-id'])) {
-//        $redcapProjectId = $module->getProjectId();
-//    }
-//    if (!isset($body['summary'])) {
-//        throw new \Exception("summary is required");
-//    }
-//
-//    if (!isset($body['type'])) {
-//        throw new \Exception("Issue Type is required");
-//    }
-//    if (!isset($body['description'])) {
-//        throw new \Exception("description is required");
-//    }
-//    $summary = '[REDCap] ' . filter_var($body['summary'], FILTER_SANITIZE_STRING);
-////    $issueType = filter_var($body['type'], FILTER_SANITIZE_NUMBER_INT);
-//    $types = $module->getSupport()->getJiraIssueTypes();
-//    $issueType = array_key_first($types);
-//    $description = filter_var($body['description'], FILTER_SANITIZE_STRING);
-//
-//    $data = $module->getSupport()->createJiraTicketViaPortal($redcapProjectId, $summary, $issueType, $description, $portalProjectId, $module->getProject()->project['app_title'], $user_firstname, $user_lastname);
-//    echo json_encode(array_merge($data, array('status' => 'success', 'message' => "<a target='_blank' href='" . $module->getClient()->getPortalBaseURL() . "support?id=" . $data['id'] . "'><h3>" . $data['jira']['key'] . "</h3></a>&nbsp;<h3>been created</h3>")));
+    $portalProjectId = $body['project_portal_id'];
+    $redcapProjectId = $body['redcap_project_id'];;
+    if (!isset($body['redcap-project-id'])) {
+        $redcapProjectId = $module->getProjectId();
+    }
+    if (!isset($body['summary'])) {
+        throw new \Exception("summary is required");
+    }
+
+    if (!isset($body['type'])) {
+        throw new \Exception("Issue Type is required");
+    }
+    if (!isset($body['description'])) {
+        throw new \Exception("description is required");
+    }
+    $summary = '[REDCap] ' . filter_var($body['summary'], FILTER_SANITIZE_STRING);
+//    $issueType = filter_var($body['type'], FILTER_SANITIZE_NUMBER_INT);
+    $types = $module->getSupport()->getJiraIssueTypes();
+    $issueType = array_key_first($types);
+    $description = filter_var($body['description'], FILTER_SANITIZE_STRING);
+
+    $data = $module->getSupport()->createJiraTicketViaPortal($redcapProjectId, $summary, $issueType, $description, $portalProjectId, $module->getProject()->project['app_title'], $user_firstname, $user_lastname);
+    echo json_encode(array_merge($data, array('status' => 'success', 'message' => "<a target='_blank' href='" . $module->getClient()->getPortalBaseURL() . "support?id=" . $data['id'] . "'><h3>" . $data['jira']['key'] . "</h3></a>&nbsp;<h3>been created</h3>")));
     echo json_encode(array('message' => 'jira ticket created '));
 } catch (\LogicException $e) {
     header("Content-type: application/json");
