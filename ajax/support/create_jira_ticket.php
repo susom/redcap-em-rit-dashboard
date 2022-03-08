c3c663d6be455ed7414c016ab124300c0c15f773
ihabz <ihab.zeedia@stanford.edu>
2022-03-08 11:03:24 -0800

    fix typo.

--- ajax/support/create_jira_ticket.php
+++ ajax/support/create_jira_ticket.php
@@ -31,7 +31,7 @@ try {
     $description = filter_var($body['description'], FILTER_SANITIZE_STRING);
 
     $data = $module->getSupport()->createJiraTicketViaPortal($redcapProjectId, $summary, $issueType, $description, $portalProjectId, $module->getProject()->project['app_title'], $user_firstname, $user_lastname);
-    echo json_encode(array_merge($data, array('status' => 'success', 'message' => "<a target='_blank' href='" . $module->getClient()->getPortalBaseURL() . "support?id=" . $data['id'] . "'><h3>" . $data['jira']['key'] . "</h3></a>&nbsp;<h3>been created</h3>")));
+    echo json_encode(array_merge($data, array('status' => 'success', 'message' => "<a target='_blank' href='" . $module->getClient()->getPortalBaseURL() . "support?id=" . $data['id'] . "'><h3>" . $data['jira']['key'] . "</h3></a>&nbsp;<h3>has been created</h3>")));
 } catch (\LogicException $e) {
     header("Content-type: application/json");
     http_response_code(404);
