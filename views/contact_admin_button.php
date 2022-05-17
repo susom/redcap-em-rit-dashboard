2bfb961379ef968964b8e5ac89b0f4cc79f025d0
ihabz <ihab.zeedia@stanford.edu>
2022-05-17 14:34:41 -0700

    fix for redcap 12.3.2 to remove top contact admin button

--- views/contact_admin_button.php
+++ views/contact_admin_button.php
@@ -10,6 +10,12 @@ namespace Stanford\ProjectPortal;
 
     var supportTab = '<br><div style="padding-top:15px;"><a href="' + dashboardUrl + '&open-support-modal=true#support" class="btn-contact-admin btn btn-danger btn-xs fs13" style="color:#fff;"><i style="color: white" class="fas fa-question-circle"></i>    Contact REDCap Support</a> </div>';
     window.onload = function () {
-        $(".btn-contact-admin").attr('href', dashboardUrl).html('<i style="color: white" class="fas fa-desktop"></i> REDCap R2P2 Dashboard').after(supportTab)
+
+
+        $(".btn-contact-admin").attr('href', dashboardUrl).html('<i style="color: white" class="fas fa-desktop"></i> REDCap R2P2 Dashboard').after(supportTab);
+
+
+        // for redcap 12.3.2 remove top contact admin button
+        $(document).find("a:contains('Contact REDCap administrator')").parent().remove()
     };
 </script>
