98b9955966ca13aa7e3513ed1ab50916db141465
ihabz <ihab.zeedia@stanford.edu>
2023-05-22 12:56:04 -0700

    include custom changes in calculating monthly charges.

--- classes/Entity.php
+++ classes/Entity.php
@@ -128,6 +128,11 @@ class Entity
             }
         }
 
+        /** @var \Stanford\ExternalModuleManager\ExternalModuleManager $manager */
+        $manager = \ExternalModules\ExternalModules::getModuleInstance('external_module_manager');
+
+        $total += $manager->getProjectTotalCustomCharges($projectId);
+
         return $total;
     }
 
