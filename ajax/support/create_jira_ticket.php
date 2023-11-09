1a607f5d7a7449f606cf054987fea652c4c3eff1
ihabz <ihab.zeedia@stanford.edu>
2023-11-09 12:08:42 -0800

    fix reporter first and last name when ticket is created on befalh of someone else.

--- ajax/support/create_jira_ticket.php
+++ ajax/support/create_jira_ticket.php
@@ -2,6 +2,7 @@
 
 namespace Stanford\ProjectPortal;
 
+use ExternalModules\ExternalModules;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 
@@ -26,9 +27,14 @@ try {
     }
 
     $raise_on_behalf_of = USERID;
+    $firstName = $user_firstname;
+    $lastName = $user_lastname;
     if (isset($body['on_behalf_of']) && $body['on_behalf_of'] != '') {
         if (defined('SUPER_USER') && SUPER_USER == 1) {
             $raise_on_behalf_of = filter_var($body['on_behalf_of'], FILTER_SANITIZE_STRING);
+            $user = ExternalModules::getUserInfo($raise_on_behalf_of);
+            $firstName = $user['user_firstname'];
+            $lastName = $user['user_lastname'];
         }
     }
 
@@ -38,7 +44,7 @@ try {
     $issueType = array_key_first($types);
     $description = filter_var($body['description'], FILTER_SANITIZE_STRING);
 
-    $data = $module->getSupport()->createJiraTicketViaPortal($redcapProjectId, $summary, $issueType, $description, $portalProjectId, $module->getProject()->project['app_title'], $raise_on_behalf_of, $user_firstname, $user_lastname);
+    $data = $module->getSupport()->createJiraTicketViaPortal($redcapProjectId, $summary, $issueType, $description, $portalProjectId, $module->getProject()->project['app_title'], $raise_on_behalf_of, $firstName, $lastName);
     echo json_encode(array_merge($data, array('status' => 'success', 'message' => "<a target='_blank' href='" . $module->getClient()->getPortalBaseURL() . "support?id=" . $data['id'] . "'><h3>" . $data['jira']['key'] . "</h3></a>&nbsp;<h3>has been created</h3>")));
 } catch (\Exception $e) {
     header("Content-type: application/json");
