ce56e5876c7b81e9a8f62d4f66314c65d8668b9f
ihabz <ihab.zeedia@stanford.edu>
2024-03-19 13:47:14 -0700

    fix array error.

--- classes/Portal.php
+++ classes/Portal.php
@@ -142,7 +142,7 @@ class Portal
             } else {
                 throw new \Exception("could not get REDCap signed auth from portal.");
             }
-            return end($projects);
+            return $projects;
         }catch (\Exception $e) {
             echo $e->getMessage();
         }
